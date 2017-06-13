5f334c1ea4fb9f02ec24aea6b37f23781d2c7646
Arash Jofrehei <arashjofrehei@yahoo.com>
2017-06-14 01:34:00 +0200

    Add files via upload

---
+++ cmake_command.sh
@@ -0,0 +1,6 @@
+mkdir cmake
+cd cmake
+mkdir ntuples
+#cmake -DGeant4_DIR=/Users/pandolf/local/geant4.10.00.p01-install/lib/Geant4-10.0.1/ ..
+#cmake -DGeant4_DIR=/afs/cern.ch/sw/lcg/external/geant4/10.3/x86_64-slc6-gcc49-opt/lib64/Geant4-10.3.0/ ..
+cmake -DGeant4_DIR=/afs/cern.ch/sw/lcg/external/geant4/10.1.p01/x86_64-slc6-gcc49-opt/lib64/Geant4-10.1.1/ ..
